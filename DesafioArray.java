487c1073b94e26ac72cef2775785d46345c81b9f
Snaymi <sneymeb@hotmail.com>
2023-03-26 19:57:50 -0300

    Troca suprema

--- DesafioArray.java
+++ DesafioArray.java
@@ -3,42 +3,73 @@ import java.util.Scanner;
 public class DesafioArray {
     public static void main (String [] args) {
 
-        int [][] matriz;
-        int opção;
-        Scanner scan = new Scanner (System.in);
-        int dia;
-        int hora;
+        Scanner scan = new Scanner(System.in);
+        int cont;
+        String resposta;
+        String confirmaçao;
         int mes;
+        String compromisso;
+        int dia;
+        String denovo;
+            System.out.print("Gostaria de marcar algum compromisso? \n");
+            resposta = scan.next();
+        do {
+            System.out.print("Diga o nome do compromisso: \n");
 
-        System.out.print("O que você quer fazer? \n 1 - Inserir compromisso \n 2 - Consultar compromisso");
-        System.out.print("\n3 - Sair");
-        opção = scan.nextInt();
+            compromisso = scan.next();
 
+            System.out.print("Para qual mês seria?\n");
+
+            System.out.print("1 - Janeiro\n");
+            System.out.print("2 - Fevereiro\n");
+            System.out.print("3 - Março\n");
+            System.out.print("4 - Abril\n");
+            System.out.print("5 - Maio\n");
+            System.out.print("6 - Junho\n");
+            System.out.print("7 - Julho\n");
+            System.out.print("8 - Agosto\n");
+            System.out.print("9 - Setembro");
+            System.out.print("10 - Outubro\n");
+            System.out.print("11 - Novembro\n");
+            System.out.print("12 - Dezembro\n ");
+            System.out.print("\nDigite aqui: ");
+
+            mes = scan.nextInt();
+
+            System.out.print("Qual seria o dia?\n");
+            dia = scan.nextInt();
+            if (mes == 2){
+              do{ 
+                if (dia<29) {
+                    System.out.print("Confirme os dados:\n");
+
+                System.out.print("Para o dia "+ dia+" do mês "+ mes +" esta marcado o " + compromisso+ " Confirma?" );
+           
+                confirmaçao = scan.next();
+                }else {
+                    System.out.print("O dia não existe\n");
+                    System.out.print("Gostaria de tentar denovo? \n")
+                    denovo = scan.next();
+                }
+            }while(denovo.equals ("Sim"));
+               
+            }
+            System.out.print("Confirme os dados:\n");
+
+            System.out.print("Para o dia "+ dia +" do mês "+ mes +" esta marcado o " + compromisso+ " Confirma?" );
+           
+            confirmaçao = scan.next();
+
+            for (cont = 0; cont >13 ; cont ++){
             
-            do{
 
-                System.out.print("Para qual mes? \n Utilize meses númericos ");
-                
-                mes = scan.nextInt();
-                
-                    if (mes >0 && mes < 13) {
-                        
-                    }
-                dia = scan.nextInt();
-
-                    if (dia>0 && dia<32){
-                        System.out.print("O dia tem 24 horas, qual horário é o primeiro compromisso?");
-                        hora = scan.nextInt();
-                            if(hora>0 && hora<25) {
-                                
-                                System.out.print("Compromisso marcado para " +hora+" horas do dia " +dia+" de "+ mes);
-                                }else{
-                                    System.out.print("hora inválida");}
-
-                        }else {
-                        System.out.print("Dia ,inválido\n");
-                         }
-
-            }while (opção =1) ;
+            }
+            
+            
+        
+        }while (resposta.equals ("sim") || resposta.equals ("Sim")|| resposta.equals ("S"));
+       
+        
     }
+
 }
